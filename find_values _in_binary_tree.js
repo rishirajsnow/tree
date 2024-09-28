d1151a59864bf12ada5bd75e96688b77a5c63dbb
rishiraj Bhowmik <rishiraj.bhowmik5@gmail.com>
2024-09-29 03:08:44 +0530

    find value in binary tree added.

---
+++ find_values _in_binary_tree.js
@@ -0,0 +1,38 @@
+// //Finding value in binary tree.
+// // //            a
+// //             /   \
+// //            b     c
+// //           / \    /\
+// //          d   e     f
+// //Command to run code :   Node file_name.js
+// class Node{
+//     constructor(val){
+//         this.val=val;
+//         this.left=null;
+//         this.right=null;
+//     }
+// }
+
+// const treeIncludes=(root,target)=>{
+//     if (root===null)return false;
+//     if(root===target)return true;
+//     return treeIncludes(root.left,target)||treeIncludes(root.right,target);
+// }
+
+
+// const a=new Node('a');
+// const b=new Node('b');
+// const c=new Node('c');
+// const d=new Node('d');
+// const e=new Node('e');
+
+
+// a.left=b;
+// a.right=c;
+// b.left=d;
+// b.right=e;
+// c.right=f;
+
+// console.log(treeIncludes(a,'e'));
+
+console.log("abc");
